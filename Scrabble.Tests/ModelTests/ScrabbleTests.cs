376d33ddee027e1d4e286995acaaa6cc2293dea1
Christine Frank and Carrie Schmid <carrie@>
2019-10-03 10:26:17 -0700

    Add tests and functions to score all letters and words.

--- Scrabble.Tests/ModelTests/ScrabbleTests.cs
+++ Scrabble.Tests/ModelTests/ScrabbleTests.cs
@@ -42,9 +42,44 @@ namespace Scrabble.Tests {
         }
 
         [TestMethod]
-        public void LetterScore_GiveLetterScore_IntValue1()
-        {
-            Assert.AreEqual(1, game.LetterScore('a'));
+        public void LetterScore_GiveLetterScore_IntValue1 () {
+            Assert.AreEqual (1, game.LetterScore ('a'));
         }
+
+        [TestMethod]
+        public void LetterScore_GiveLetterScore_IntValue2 () {
+            Assert.AreEqual (2, game.LetterScore ('d'));
+        }
+
+        [TestMethod]
+        public void LetterScore_GiveLetterScore_IntValue3 () {
+            Assert.AreEqual (3, game.LetterScore ('b'));
+        }
+
+        [TestMethod]
+        public void LetterScore_GiveLetterScore_IntValue4 () {
+            Assert.AreEqual (4, game.LetterScore ('f'));
+        }
+
+        [TestMethod]
+        public void LetterScore_GiveLetterScore_IntValue5 () {
+            Assert.AreEqual (5, game.LetterScore ('k'));
+        }
+
+        [TestMethod]
+        public void LetterScore_GiveLetterScore_IntValue8 () {
+            Assert.AreEqual (8, game.LetterScore ('j'));
+        }
+
+        [TestMethod]
+        public void LetterScore_GiveLetterScore_IntValue10 () {
+            Assert.AreEqual (10, game.LetterScore ('z'));
+        }
+
+        [TestMethod]
+        public void WordScore_GiveWordScore_Int() {
+            Assert.AreEqual (10, game.WordScore ("scary"));
+        }
+
     }
 }
--- Scrabble/Models/Scrabble.cs
+++ Scrabble/Models/Scrabble.cs
@@ -1,43 +1,61 @@
 using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using System.Linq;
+using System.Text.RegularExpressions;
 
-namespace Scrabble.Models
+namespace Scrabble.Models 
 {
-    public class Game {
-        Dictionary<int,char[]> LetterScores {get;}
-        public Game()
+    public class Game 
+    {
+        Dictionary<int, char[]> LetterScores { get; }
+        public Game () 
         {
-            LetterScores = new Dictionary<int, char[]>();
-            LetterScores.Add(1, new char[]{'a','e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'});
+            LetterScores = new Dictionary<int, char[]> ();
+            LetterScores.Add (1, new char[] { 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't' });
+            LetterScores.Add (2, new char[] { 'd', 'g' });
+            LetterScores.Add (3, new char[] { 'b', 'c', 'm', 'p' });
+            LetterScores.Add (4, new char[] { 'f', 'h', 'v', 'w', 'y' });
+            LetterScores.Add (5, new char[] { 'k' });
+            LetterScores.Add (8, new char[] { 'j', 'x' });
+            LetterScores.Add (10, new char[] { 'q', 'z' });
         }
 
-        public bool CheckInput(string input)
+        public bool CheckInput (string input) 
         {
-            Regex reg = new Regex(@"[^A-Za-z]");
-            if(reg.IsMatch(input))
+            Regex reg = new Regex (@"[^A-Za-z]");
+            if (reg.IsMatch (input)) 
             {
-               return false; 
+                return false;
             }
             return true;
         }
 
-        public string CheckMultipleWords(string input)
+        public string CheckMultipleWords (string input) 
         {
-            input = input.Trim();
-            if(input.IndexOf(" ")!= -1)
+            input = input.Trim ();
+            if (input.IndexOf (" ") != -1) 
             {
-                return input.Substring(0, input.IndexOf(" "));
+                return input.Substring (0, input.IndexOf (" ")).ToLower();
             }
-            
-            return input; 
+
+            return input.ToLower();
         }
 
-        public int LetterScore(char letter)
+        public int LetterScore (char letter) 
         {
-            int score = LetterScores.Where(pair => pair.Value.Contains(letter)).FirstOrDefault().Key;
+            int score = LetterScores.Where (pair => pair.Value.Contains (letter)).FirstOrDefault().Key;
             return score;
+        }
+
+        public int WordScore(string word)
+        {
+            int totalScore = 0;
+            foreach(char letter in word)
+            {
+                totalScore = totalScore + LetterScore(letter);
+            }
+            return totalScore;
+
         }
 
     }
